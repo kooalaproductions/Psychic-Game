b623071a865f580392d19b3f5055c72b91585347
Sky <erniesanchez619@gmial.com>
2019-07-08 17:41:07 -0700

    adding index.html and css folder

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-// var computerChoices = ["r", "p", "s"];
+//Psychic Game Function
 
-var left = 9;
-var wins = 0;
-var loses = 0;
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+var left = 9;//how many chances are left
+var wins = 0;//keep score of wins
+var loses = 0;//keep score of losses
+//array of alphabet
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
+//array for user choices
 var uGuess = [];
 
 
@@ -19,8 +19,9 @@ function startUp() {
     var guessUpdate = document.getElementById("guessLeft");
     guessUpdate.innerHTML = ("Guesses Left: " + left);
 
-    // var userGuess = document.getElementById("leftGuess");
-    // userGuess.innerHTML = ("Your Gusses so far: " + uGuess.join(', '));
+    var userGuess = document.getElementById("leftGuess");
+    //joins the elements together
+    userGuess.innerHTML = ("Your Gusses so far: " + uGuess.join(', '));
 
 
 }
@@ -28,51 +29,64 @@ function startUp() {
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
 
+    
     // Make any character into lower case
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     //   console.log(userGuess);
-    
-    console.log("User answer: " + uGuess);
 
-    //MIGHT WORK
-    //   for(var i = 0; i<9; i++ ){
-    //       uGuess[i]=userGuess;
-    //       console.log("User Guess: " + uGuess[i])
+    console.log("User answer: " + userGuess);
 
-
-    //   }
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log("computer guess: " + computerGuess);
 
-    
+    //checks array to see if any of the keys match with the array and if so return true
     var check = computerChoices.includes(userGuess);
-
-
-    
-
+    //pushes elements to uGuess array
+    uGuess.push(userGuess);
 
     //this will run when user and computer match
-    if(check === false){
+    if (check === false) {
         alert("That was not a valid guess, try again?");
         return false;
     }
+    //run if conditions are true
     if (left !== 0 && check === true) {
-        
+
 
         if (userGuess === computerGuess) {
             alert("This is a Win");
             wins = wins + 1;
+            left = 10;//it displays 1 less than the number assigned 
+            uGuess = [];
+            // reset();
             console.log(wins);
-
-        } else {
-            left = left - 1;
+            if(uGuess.length !== 0){
+                uGuess = [];
+            }
 
         }
-        uGuess.push(userGuess);
-        
+        //run if does not match computer choice
+        if (userGuess !== computerChoices) {
+            left--;
+            //will run once there are not more tries left
+            if (left === 0) {
+                alert("Game is over! Try Again!");
+                loses = loses + 1;
+                left = 9;
+                uGuess = [];
 
+                if(uGuess.length !== 0){
+                    uGuess = [];
+                }
+   
+            }
+
+
+        }
+        
+       //updates the HTML 
         var winUpdate = document.getElementById("winsText");
         winUpdate.innerHTML = ("Wins: " + wins);
 
@@ -83,20 +97,7 @@ document.onkeyup = function (event) {
         guessUpdate.innerHTML = ("Guesses Left: " + left);
 
         var userGuess = document.getElementById("leftGuess");
+        //joins the elements together 
         userGuess.innerHTML = ("Your Gusses so far: " + uGuess.join(', '));
-
-
-
-    } 
-    
-    
-    else { //if there are no more guesses reload the page
-
-        alert("Game is over! Try Again!");
-        location.reload();
-
-
-
-
     }
 }
